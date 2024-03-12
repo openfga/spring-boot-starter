c4c2235ed40a00a3252c8fa6664a83e3cb0373b5
Jim Anderson <jim.anderson@okta.com>
2024-03-12 15:30:57 -0500

    add stubs for servlet and webflux examples

---
+++ examples/webflux/src/main/java/dev/openfga/example/WebfluxFgaApp.java
@@ -0,0 +1,12 @@
+package dev.openfga.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxFgaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxFgaApp.class, args);
+    }
+}
